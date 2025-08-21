a9494020d65626ce0850e850b5ab8706d423228a
Felipe Valenzuela <feelipevalenzuela@hotmail.com>
2025-08-20 22:05:51 -0300

    Fix ejercicios solucion

---
+++ Clase 2/ejercicios/Ejercicio1Copy.js
@@ -0,0 +1,43 @@
+
+// identificar palindromos
+
+
+const palabras = ["Sol", "Ana", "Oso", "Saludo", "Reconocer", "Soldado", "Neuquen"]
+
+function invertirTexto(palabra) {
+    // aca se invierte el texto
+    const palabraDividida = palabra.split('')
+
+    console.log("test");
+
+    console.log([palabraDividida]);
+    console.log([...palabraDividida]);
+
+
+    const palabraDivididaAlRevez = [...palabraDividida].reverse();
+    for (let i = 0; i < palabraDividida.length; i++) {
+        if (palabraDividida[i] !== palabraDivididaAlRevez[i]) {
+            return false
+        }
+    }
+    return true
+}
+
+function esPalindromo(palabras) {
+
+    const resultado = []
+
+    for (const element of palabras) {
+        let palabra = element.toLowerCase()
+        if (invertirTexto(palabra)) {
+            resultado.push(palabra)
+        }
+    }
+
+    return resultado
+}
+
+const resultado = esPalindromo(palabras)
+
+console.log("Palabras Palindromo: ", resultado);
+
